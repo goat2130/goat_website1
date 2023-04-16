ec13060defc9bbe52f2c0993b7f68a16b7b6b51a
Goat <goat229@icloud.com>
2023-04-16 15:32:10 +0000

    middlewares.py, htmlファイルを追加

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -4,6 +4,7 @@ from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.views import View
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 from .forms import LoginForm
