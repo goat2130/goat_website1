f584f34a6b4d161ecce5c5062d699624a80f3f0e
Goat <goat229@icloud.com>
2023-04-18 12:40:29 +0000

    微調整

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -4,9 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-from . import views
-
-# from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
